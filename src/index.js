696b42f6a306a4933fec6e729cff7d2d9ed4d848
Diana Yankovskaya <dianashpandaruk@mail.ru>
2021-01-29 14:20:46 +0300

    feat: implement min function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  return array === undefined || array.length === 0 ? 0 : array.reduce((minNum, currentNum) => minNum > currentNum ? currentNum : minNum);
 }
 
 exports.max = function max (array) {

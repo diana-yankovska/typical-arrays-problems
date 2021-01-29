90c8efa4b350949b340ce60fdc3cbb4f76ca2113
Diana Yankovskaya <dianashpandaruk@mail.ru>
2021-01-29 14:23:31 +0300

    feat: implement max function

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ exports.min = function min(array) {
   return array === undefined || array.length === 0 ? 0 : array.reduce((minNum, currentNum) => minNum > currentNum ? currentNum : minNum);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  return array === undefined || array.length === 0 ? 0 : array.reduce((maxNum, currentNum) => maxNum > currentNum ? maxNum : currentNum);
 }
 
 exports.avg = function avg (array) {
